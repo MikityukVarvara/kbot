6158ca648f76dd9f7e57b1391fa32dcc706f43c0
Mikityuk Varvara <mikityuk.varvara@gmail.com>
2024-04-20 20:54:58 +0300

    Update version.go

--- cmd/version.go
+++ cmd/version.go
@@ -28,7 +28,6 @@ to quickly create a Cobra application.`,
 
 func init() {
 	rootCmd.AddCommand(versionCmd)
-
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
